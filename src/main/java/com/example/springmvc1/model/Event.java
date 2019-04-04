ddab5c07b142a371d3ec3c5f3acfd4936d064c45
godchiken <funnic2@gmail.com>
2019-04-04 18:02:04 +0900

    스프링 동작 원리 - 스프링 MVC 소개

---
+++ src/main/java/com/example/springmvc1/model/Event.java
@@ -0,0 +1,17 @@
+package com.example.springmvc1.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private String name;
+    private int limitOfEnrollment;
+    private LocalDateTime startLocalDateTime;
+    private LocalDateTime endLocalDateTime;
+}
