a9b81c10748ffc7c5ad2612a68c053852b4f8d80
godchiken <funnic2@gmail.com>
2019-04-05 16:45:32 +0900

    close #1 백기선 스프링 웹 MVC - 스프링 MVC 소개 - 수강완료

--- src/main/java/com/example/springmvc1/service/EventService.java
+++ src/main/java/com/example/springmvc1/service/EventService.java
@@ -23,7 +23,7 @@ public class EventService {
 				.startLocalDateTime(LocalDateTime.of(2019, 1, 10, 10, 0))
 				.endLocalDateTime(LocalDateTime.of(2019, 1, 10, 12, 0))
 				.build();
-		/*TODO: WIKI -> Java 9 이상에서는 List.of() 메서드를 활용하여 불변객체를 생성한다. 10개까지 가능*/
+
 		List list = new ArrayList();
 		list.add(event);
 		list.add(event2);
